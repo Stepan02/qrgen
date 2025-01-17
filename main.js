08db92e55b9c7ca6fcd7c4574d63e462dc01167f
Stepan02 <85454063+Stepan02@users.noreply.github.com>
2025-01-17 14:52:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function download() {
+    window.location.href = 'v1/Download/v1.zip'; /* UDĚLAT ZIP VERZE 1 */
+}
+function changelog() {
+    window.location.href = 'changelog.htm';
+}
+function tutorial() {
+    window.location.href = 'tutorial.htm';
+}
+function demo() {
+    const modal = document.querySelector('#demo');
+        modal.showModal();
+}
+function home() {
+    window.location.href = 'main.htm';
+}
+
+const clippymodal = document.querySelector('#clippy');
+clippymodal.onkeydown = function( event ) {
+    if ( event.keyCode == 27 ) {
+        event.preventDefault();
+    }
+};
+function clippy() {
+    const clippymodal = document.querySelector('#clippy');
+        clippymodal.showModal()
+        document.querySelector("body").style.overflow = 'hidden';
+}
+function copyClippy() {
+    navigator.clipboard.writeText('📎').then(() => alert('🎉 Copied, enjoy! 🎉'))
+}
