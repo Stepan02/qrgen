0db0beb52f3d3638052fa1f92294248591647569
Stepan02 <85454063+Stepan02@users.noreply.github.com>
2025-05-07 21:33:11 +0200

    Demo dialog update
    - The QR code image is downloaded when the user clicks on it

--- demo/main.js
+++ demo/main.js
@@ -10,6 +10,32 @@ if(!value || value === preValue) return
 preValue = value
 qrCode.src =` https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${value} `
 })
+
+qrCode.style.cursor = "pointer";
+
+generateBtn.addEventListener("click", () => {
+    let value = inputValue.value.trim();
+    if (!value || value === preValue) return;
+    preValue = value;
+    qrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${value}`;
+});
+
+// download function
+qrCode.addEventListener("click", () => {
+    const imageUrl = qrCode.src;
+    if (!imageUrl) return;
+
+    fetch(imageUrl)
+        .then(response => response.blob())
+        .then(blob => {
+            const link = document.createElement("a");
+            link.href = URL.createObjectURL(blob);
+            link.download = "qrcode.png";
+            link.click();
+        })
+        .catch(error => console.error("Error downloading an image:", error));
+});
+
 function updateCounter() {
     const textarea = document.querySelector("textarea");
     const counter = document.getElementById("char-counter");
