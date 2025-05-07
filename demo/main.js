24922d88a946d70356e98555ac185c75282dadba
Stepan02 <85454063+Stepan02@users.noreply.github.com>
2025-05-07 21:43:17 +0200

    Duplicate generateBtn EventListener fix
    - Removed duplicate EventListener of generateBtn accidentally added in 0db0beb

--- demo/main.js
+++ demo/main.js
@@ -4,13 +4,6 @@ qrCode = document.querySelector(".qr-code img"),
 container = document.querySelector(".container")
 let preValue
 
-generateBtn.addEventListener("click", () => {
-let value = inputValue.value.trim() 
-if(!value || value === preValue) return
-preValue = value
-qrCode.src =` https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${value} `
-})
-
 qrCode.style.cursor = "pointer";
 
 generateBtn.addEventListener("click", () => {
