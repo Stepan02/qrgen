9d204b10383b97d9afa665e73457f4afb2ee84b8
Stepan02 <85454063+Stepan02@users.noreply.github.com>
2025-05-20 22:18:43 +0200

    Added retries for tests
    - Added 2 retries for tests to handle occasional failures due to slow CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:5500",
-    setupNodeEvents(on, config) {
-      // add more events if needed
-    },
-  },
+  e2e: {
+    baseUrl: "http://localhost:5500",
+    setupNodeEvents(on, config) {
+      // add more events if needed
+    },
+  },
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
 });
