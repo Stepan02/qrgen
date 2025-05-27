efb68925f744c1e366c66fe8629a71a808314f1c
Stepan02 <85454063+Stepan02@users.noreply.github.com>
2025-05-27 23:02:47 +0200

    Test optimization and typo fix
    - Fixed typo in "Generates QR code" - "if" -> "it" - typo caused the test to not run
    - Added aliases for reused elements
    - Optimized testing of unsafe protocols

--- cypress/e2e/qrapi.cy.js
+++ cypress/e2e/qrapi.cy.js
@@ -2,79 +2,76 @@ describe('QR API test', () => {
   beforeEach(() => {
     cy.visit('http://127.0.0.1:5500/main.htm');
     cy.get('#openmodal').click();
+
+    // aliases
+    cy.get('textarea').as('input');
+    cy.get('.generateBtn').as('generateBtn');
+    cy.get('.qr-code img').as('qrImg');
   });
 
+  // function to generate a QR code
+  const generateQR = (text) => {
+    cy.get('@input').clear().type(text);
+    cy.get('@generateBtn').click();
+  };
+
   it('Does not generate a blank QR code', () => {
-    cy.get('.qr-code img').should('have.attr', 'src', '');
+    cy.get('@qrImg').should('have.attr', 'src', '');
 
-    cy.get('.generateBtn').click();
+    cy.get('@generateBtn').click();
     
-    cy.get('.qr-code img').should('have.attr', 'src', '');
-  })
+    cy.get('@qrImg').should('have.attr', 'src', '');
+  });
 
-  if('Generates QR code', () => {
+  it('Generates QR code', () => {
     const testUserInput = 'Hello World!';
+    generateQR(testUserInput);
 
-    cy.get('textarea').clear().type(testUserInput);
-    cy.get('.generateBtn').click();
-
-    cy.get('.qr-code img').should('have.attr', 'src').and('include', encodeURIComponent(testUserInput));
+    cy.get('@qrImg')
+      .should('have.attr', 'src')
+      .and('include', encodeURIComponent(testUserInput));
   });
 
   it('Does not repeat when the input stays the same', () => {
-      const testText = 'Hello World!';
+    const testText = 'Hello World!';
+    generateQR(testText);
 
-      cy.get('textarea').clear().type(testText);
-      cy.get('.generateBtn').click();
-      cy.get('.qr-code img').invoke('attr', 'src').then((src1) => {
-        cy.get('.generateBtn').click();
-        cy.get('.qr-code img').invoke('attr', 'src').should('eq', src1);
-      });
+    cy.get('@qrImg').invoke('attr', 'src').then((src1) => {
+      cy.get('@generateBtn').click();
+      cy.get('@qrImg').invoke('attr', 'src').should('eq', src1);
     });
+  });
 
   it('Character counter updates', () => {
-    cy.get('textarea').clear().type('abcd');
+    cy.get('@input').clear().type('abcd');
     cy.get('#char-counter').should('have.text', '4');
   });
 
   it('Does not overflow', () => {
-    cy.get('textarea').clear().type('qwerty');
-    cy.get('.generateBtn').click();
-
+    const safeText = 'qwerty';
     const overflow = 'A'.repeat(2500);
-    
-    cy.get('.qr-code img')
+
+    generateQR(safeText);
+
+    cy.get('@qrImg')
       .should('be.visible')
       .invoke('attr', 'src')
       .then((initialSrc) => {
-        cy.get('textarea').clear().invoke('val', overflow).trigger('input');
-        cy.get('.generateBtn').click();
+        cy.get('@input').clear().invoke('val', overflow).trigger('input');
+        cy.get('@generateBtn').click();
 
-        cy.get('.qr-code img')
+        cy.get('@qrImg')
           .invoke('attr', 'src')
           .should('eq', initialSrc);
       });
-  })
+  });
 
-  it('Does not generate QR codes with dangerous protocols', () => {
-    cy.get('textarea').clear().invoke('val', 'javascript:').trigger('input');
-    cy.get('.generateBtn').click();
-    
-    cy.get('.qr-code img').should('not.be.visible');
+  ['javascript:', 'data:', 'file:', 'vbscript:'].forEach((protocol) => {
+    it(`Does not generate QR codes with dangerous protocol: ${protocol}`, () => {
+      cy.get('@input').clear().invoke('val', protocol).trigger('input');
+      cy.get('@generateBtn').click();
 
-    cy.get('textarea').clear().invoke('val', 'data:').trigger('input');
-    cy.get('.generateBtn').click();
-    
-    cy.get('.qr-code img').should('not.be.visible');
-
-    cy.get('textarea').clear().invoke('val', 'file:').trigger('input');
-    cy.get('.generateBtn').click();
-    
-    cy.get('.qr-code img').should('not.be.visible');
-
-    cy.get('textarea').clear().invoke('val', 'vbscript:').trigger('input');
-    cy.get('.generateBtn').click();
-    
-    cy.get('.qr-code img').should('not.be.visible');
-  })
-})
+      cy.get('@qrImg').should('not.be.visible');
+    });
+  });
+});
