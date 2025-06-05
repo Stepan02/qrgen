da29c2bb365adf8de74053950f178fc054b65602
Stepan02 <85454063+Stepan02@users.noreply.github.com>
2025-06-05 23:15:06 +0200

    Unexpected key "environments" fix
    - Fixed the ConfigError of non-existent environments config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -5,21 +5,32 @@ module.exports = [
   eslintRecommended.configs.recommended,
   {
     plugins: {
-      cypress: cypress,
+      cypress,
     },
     files: ["**/*.js"],
     languageOptions: {
       ecmaVersion: 12,
       sourceType: "module",
+      globals: {
+        cy: true,
+        Cypress: true,
+        describe: true,
+        it: true,
+        before: true,
+        beforeEach: true,
+        after: true,
+        afterEach: true,
+        context: true,
+        expect: true,
+      },
     },
-    environments: ["cypress"],
     rules: {
       "no-unused-vars": "warn",
       "no-console": "off",
-      "eqeqeq": "error",
-      "curly": "error",
-      "semi": ["error", "always"],
-      "quotes": ["error", "double"],
+      eqeqeq: "error",
+      curly: "error",
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
     },
   },
 ];
