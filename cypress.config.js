322c516d24c8a48c5e6f8e44c38096ff20536ad1
Stepan02 <85454063+Stepan02@users.noreply.github.com>
2025-06-11 23:35:58 +0200

    Code optimization
    - Removed unused setupNodeEvents
    - Removed irregular whitespace

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:5500",
-    setupNodeEvents(on, config) {
-      // add more events if needed
-    },
-  },
-  retries: {
-    runMode: 2,
-    openMode: 0,
-  },
+  e2e: {
+    baseUrl: "http://localhost:5500",
+  },
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
 });
