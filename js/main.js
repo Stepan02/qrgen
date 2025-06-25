1d1432e2b30424ab954ee0aaf699fc42cae1d862
Stepan02 <85454063+Stepan02@users.noreply.github.com>
2025-06-25 22:42:15 +0200

    Removed legacy /* exported */ comment
    - The /* exported */ comment for declaring exported JavaScript function is no longer supported in ESLint 9

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-/* exported download, changelog, tutorial, demo, home, clippy, copyClippy */
 var jQueryScript = document.createElement("script");
 jQueryScript.setAttribute("src","https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js");
 document.head.appendChild(jQueryScript);
