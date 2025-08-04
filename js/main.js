f14ac604ea0ffcd95de10a77ebdf91cb9ea89167
Stepan02 <85454063+Stepan02@users.noreply.github.com>
2025-08-04 18:01:39 +0200

    UX and code optimization
    - The Clippy modal can be closed with an escape key
    - Added comments 
    - Improved code formatting

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// jquery import
 var jQueryScript = document.createElement("script");
 jQueryScript.setAttribute("src","https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js");
 document.head.appendChild(jQueryScript);
@@ -5,48 +6,58 @@ document.head.appendChild(jQueryScript);
 function download() {
     window.location.href = "https://github.com/Stepan02/qrgen/tree/main/extension";
 }
+
 function changelog() {
     window.location.href = "changelog.htm";
 }
+
 function tutorial() {
     window.location.href = "tutorial.htm";
 }
+
 function demo() {
-    const modal = document.querySelector("#demo");
+    const modal = document.querySelector("#demo"),
+        exitText = document.querySelector(".head p");
+
+    // open the modal
     modal.showModal();
+
+    // disable scrolling when the modal is open
     $("body").css({"overflow":"hidden"});
-    let exitText = document.querySelector(".head p");
-    exitText.addEventListener("click", () => {
-        modal.close();
-   });
-   modal.addEventListener("close", () => {
-    $(document).off("scroll"); 
-    $("body").css({"overflow":"visible"});
+
+    // close the modal when the user clicks the exit text
+    exitText.addEventListener("click", modal.close);
+
+    // re-enable scrolling when the modal is opened
+    modal.addEventListener("close", () => {
+        $(document).off("scroll"); 
+        $("body").css({"overflow":"visible"});
    });
 }
+
 function home() {
     window.location.href = "main.htm";
 }
-const clippymodal = document.querySelector("#clippy");
-if (clippymodal !== null) {
-    clippymodal.onkeydown = function( event ) {
-        if ( event.keyCode === 27 ) {
-            event.preventDefault();
-        }
-    };
-}
+
 function clippy() {
+    const clippymodal = document.querySelector("#clippy");
+
+    // open the modal
     clippymodal.showModal();
-    document.querySelector("body").style.overflow = "hidden";
+    document.querySelector("body").style.overflow = "hidden"; // disable scrolling when the modal is opened
 }
+
 function copyClippy() {
     const button = document.querySelector("#clippy .normal");
+    
+    // copy the emoji to the user's clipboard
     navigator.clipboard.writeText("📎").then(() => button.innerHTML = "📋 Copied!");
 }
 
-const url = document.location.pathname;
-const place = document.querySelector(".url");
+// show current url endpoint on the 404 page
+const url = document.location.pathname,
+    place = document.querySelector(".url");
 
 if (place) {
-    place.innerHTML = url;
+    place.innerHTML = url; // show the url if place element exists
 }
