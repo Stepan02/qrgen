fa1ff6c5885a8be2b8f289a98b44771f60dcb0f4
Stepan02 <85454063+Stepan02@users.noreply.github.com>
2025-10-16 22:07:31 +0200

    Code optimization
    - Added comments

--- cypress/e2e/ext.cy.js
+++ cypress/e2e/ext.cy.js
@@ -14,14 +14,14 @@ describe("QR API test", () => {
     cy.get(".con-error-mess").as("connectionErrorMessage");
     cy.get("#char-counter").as("characterCounter");
   });
-  
+
   it("Does not generate a blank QR code", () => {
     cy.get("@qrImg").should("have.attr", "src", "");
 
     cy.get("@generateBtn").click();
     
-    cy.get("@qrImg").should("have.attr", "src", "");
-    cy.get("@downloadLink").should("not.be.visible");
+    cy.get("@qrImg").should("have.attr", "src", "");  // the qr code should not appear
+    cy.get("@downloadLink").should("not.be.visible"); // no download link and error messages should appear
     cy.get("@errorMessage").should("not.be.visible");
     cy.get("@connectionErrorMessage").should("not.be.visible");
   });
@@ -31,17 +31,17 @@ describe("QR API test", () => {
     cy.generate(testUserInput);
 
     cy.get("@qrImg")
-      .should("have.attr", "src")
-      .and("include", encodeURIComponent(testUserInput));
-    cy.get("@downloadLink").should("be.visible");
-    cy.get("@errorMessage").should("not.be.visible");
+      .should("have.attr", "src")                         // the qr code should appear
+      .and("include", encodeURIComponent(testUserInput)); // the user input should be part of the img source
+    cy.get("@downloadLink").should("be.visible");         // the download link should be visible
+    cy.get("@errorMessage").should("not.be.visible");     // no errors should be visible
   });
 
   it("Generates QR code by pressing Shift+Enter", () => {
     const input = "Hello World!{shift+enter}";
 
     cy.get("@input").clear().type(input);
-    cy.get("@qrImg").should("be.visible");
+    cy.get("@qrImg").should("be.visible"); // the img should appear
   });
 
   it("Does not repeat when the input stays the same", () => {
@@ -51,10 +51,10 @@ describe("QR API test", () => {
     cy.get("@qrImg").invoke("attr", "src").then((src1) => {
       cy.get("@generateBtn").click();
 
-      cy.get("@qrImg").invoke("attr", "src").should("eq", src1);
-      cy.get("@downloadLink").should("be.visible");
+      cy.get("@qrImg").invoke("attr", "src").should("eq", src1);  // first qr codew should appear
+      cy.get("@downloadLink").should("be.visible");               // the download link should be visible
 
-      cy.get("@errorMessage").should("not.be.visible");
+      cy.get("@errorMessage").should("not.be.visible");           // no errors should be visible
       cy.get("@connectionErrorMessage").should("not.be.visible");
     });
   });
@@ -70,15 +70,16 @@ describe("QR API test", () => {
 
     cy.generate(safeText);
 
-    cy.get("@characterCounter").should("not.have.css", "color", "rgb(233, 74, 132)");
+    cy.get("@characterCounter").should("not.have.css", "color", "rgb(233, 74, 132)"); // the character counter should not turn red
 
-    cy.get("@input").clear().invoke("val", maxLenghtText).trigger("input");
+    cy.get("@input").clear().invoke("val", maxLenghtText).trigger("input");           // brute force input over the limit
     cy.get("@generateBtn").click();
 
-    cy.get("@characterCounter").should("have.have.css", "color", "rgb(233, 74, 132)").and("have.css", "font-weight", "900");
+    cy.get("@characterCounter").should("have.have.css", "color", "rgb(233, 74, 132)") // the character counter should be red
+                                .and("have.css", "font-weight", "900");               // and bold 
 
     cy.generate(safeText);
-    cy.get("@characterCounter").should("not.have.css", "color", "rgb(233, 74, 132)");
+    cy.get("@characterCounter").should("not.have.css", "color", "rgb(233, 74, 132)"); // the character counter should not be red
   });
 
   it("Does not overflow", () => {
@@ -88,20 +89,20 @@ describe("QR API test", () => {
     cy.generate(safeText);
 
     cy.get("@qrImg")
-      .should("be.visible")
-      .invoke("attr", "src")
+      .should("be.visible") 
+      .invoke("attr", "src")                                               // the qr code should be visible and have content
       .then((initialSrc) => {
-        cy.get("@input").clear().invoke("val", overflow).trigger("input");
+        cy.get("@input").clear().invoke("val", overflow).trigger("input"); // force the input over the limit
         cy.get("@generateBtn").click();
 
         cy.get("@qrImg")
           .invoke("attr", "src")
           .should("eq", initialSrc);
-        cy.get("@errorMessage").should("not.be.visible");
-        cy.get("@downloadLink").should("not.be.visible");
+        cy.get("@errorMessage").should("not.be.visible");           // no errors should be visible
         cy.get("@connectionErrorMessage").should("not.be.visible");
+        cy.get("@downloadLink").should("not.be.visible");           // the download link should not be visible
       });
-    cy.get("@errorMessage").should("not.be.visible");
+    cy.get("@errorMessage").should("not.be.visible");               // no errors should be visible
     cy.get("@connectionErrorMessage").should("not.be.visible");
   });
 
@@ -122,11 +123,11 @@ describe("QR API test", () => {
       cy.get("@input").clear().invoke("val", input).trigger("input");
       cy.get("@generateBtn").click();
 
-      cy.get("@qrImg").should("not.be.visible");
+      cy.get("@qrImg").should("not.be.visible");                             // the qr code should not generate
 
-      cy.get("@downloadLink").should("not.be.visible");
-      cy.get("@errorMessage").should("be.visible").and("contain", expected);
-      cy.get("@connectionErrorMessage").should("not.be.visible");
+      cy.get("@downloadLink").should("not.be.visible");                      // the download link should not be visible
+      cy.get("@errorMessage").should("be.visible").and("contain", expected); // the error message should appear
+      cy.get("@connectionErrorMessage").should("not.be.visible");            // no connection error message should be visible
     });
   });
 });
