74e128059c60da407201512264f05973b6e1fd2d
Stepan02 <85454063+Stepan02@users.noreply.github.com>
2025-12-26 15:25:39 +0100

    QR code regeneration conditions change
    - The QR code can be regenerated if the input data remained the same but the color has changed

--- extension/popup.js
+++ extension/popup.js
@@ -17,9 +17,12 @@ let limit = 2000,
 function generate() {
     const value = inputValue.value.trim(),
           color = qrCodeColor.value.substring(1, 7); // remove # from the hex code
+    
+    // does not generate on empty input
+    if (!value) { return; }
 
-    // does not regenerate on empty input or if the input and the color stay the same
-    if ((!value || value === previousValue) && color === previousColor) { return; }
+    // does not regenerate if the input and the color stay the same
+    if (value === previousValue && color === previousColor) { return; }
 
     // does not generate when the input is over the character limit
     if (limit > 0 && value.length > limit) { return; }
