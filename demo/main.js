f517f3d045900209f38327fcf9a99a0cfe43d331
Stepan02 <85454063+Stepan02@users.noreply.github.com>
2026-02-21 22:42:32 +0100

    Backticks replaced by doublequotes, enforced maxium line length

--- demo/main.js
+++ demo/main.js
@@ -83,9 +83,10 @@ function generate() {
     let rgbColor           = convertHexToRgb(color);
     let rgbBackgroundColor = convertHexToRgb(backgroundColor);
 
-    // add a warning if the user generated a qr code with bad color contrast - the contrast ratio should be over 4.5 (https://www.w3.org/WAI/WCAG21/Techniques/general/G174)
+    // add a warning if the user generated a qr code with bad color contrast 
+    // the contrast ratio should be over 4.5 (https://www.w3.org/WAI/WCAG21/Techniques/general/G174)
     if (getContrastRatio(rgbColor, rgbBackgroundColor) < 4.5) {
-        imageContrastWarning.textContent = `This color combination might render the QR code unreadable.`;
+        imageContrastWarning.textContent = "This color combination might render the QR code unreadable.";
 
         imageContrastWarning.style.display = "block";
 
